ee7d16826c1f25fa8478e4a354b2aafa9d4359a7
kanonenfutter <van.kanonenfutter@web.de>
2016-05-30 13:36:57 +0200

    + communication between node.js and python
    Problem: Print Nachrichten werden gesammelt ausgegeben

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var pythonshell = require('python-shell');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Coffeefy' });
 });
 
+router.get('/test', function(req, res, next){
+    res.render('test');
+});
+
+router.get('/scripts/1/', function(req, res, next){
+    pythonshell.run('../python/helloworld.py', function(err, results){
+        if (err) throw err;
+        console.log('results: %j', results);
+    });
+    res.render('test');
+    res.end();
+});
+
+
+router.get('/scripts/2/', function(req, res, next){
+    runscript();
+
+    res.end();
+});
+
 module.exports = router;
+
+function runscript(){
+    var pyshell = new pythonshell('../python/exampleTimedOutputs.py');
+    pyshell.on('message', function (message){
+        console.log(message);
+    });
+};
+    
