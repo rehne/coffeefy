541f4f4ad2ac4effd12b572a16b1c763c9137862
kanonenfutter <van.kanonenfutter@web.de>
2016-05-30 13:50:11 +0200

    typos, license

--- node/app.js
+++ node/app.js
@@ -1,3 +1,13 @@
+// Copyright (c) 2016 by 
+// [Vanakh Chea](https://github.com/kanonenfutter/), 
+// [Alexander Thürling](https://github.com/athuerli), 
+// [René Honnen](https://github.com/rehne)
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
