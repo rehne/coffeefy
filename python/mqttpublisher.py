f8235949bbd6dfe328cb1fada287bd97f0522ba9
Vanakh Chea <van.kanonenfutter@web.de>
2016-06-02 19:26:47 +0200

    fix

--- python/mqttpublisher.py
+++ python/mqttpublisher.py
@@ -4,4 +4,5 @@ import paho.mqtt.client as mqtt
 mqttc = mqtt.Client("python_pub")
 mqttc.connect("iot.eclipse.org", 1883, 60)
 mqttc.publish("coffeefy", "Hello, World!")
-mqttc.loop(2) //timeout = 2s
+#timeout = 2s
+mqttc.loop(2)
