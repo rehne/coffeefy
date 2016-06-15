e5597c179100072e34801272774b02cb201ecb57
kanonenfutter <van.kanonenfutter@web.de>
2016-06-15 15:33:22 +0200

    gpio pin settings

--- python/powerbtn.py
+++ python/powerbtn.py
@@ -10,6 +10,11 @@ mqttc.connect("iot.eclipse.org", 1883, 60)
 
 GPIO.setmode(GPIO.BCM)
 
+# set up GPIO pins
+GPIO.setup(4, GPIO.OUT)
+# set trigger to false
+GPIO.output(4, False)
+
 GPIO.output(4, True)
 time.sleep(0.01)
 GPIO.output(4, False)
