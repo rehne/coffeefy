82f8dfce0a6243be1edf110d1edc39a410c37594
rehne <rene.honnen@smail.th-koeln.de>
2016-06-24 16:36:36 +0200

    mqtt.publsh() -> client.publish()

--- node/routes/index.js
+++ node/routes/index.js
@@ -8,7 +8,7 @@ var data;
 
 /* GET resource "/" aka homepage */
 router.get('/', function(req, res, next) {
-    //data = readUltrasonicSensor();
+    data = readUltrasonicSensor();
     res.render('index', { title: 'Coffeefy', distance: data });
     res.end();
 });
@@ -65,13 +65,13 @@ function pressPowerButton(){
   gpio.setup(19, gpio.DIR_OUT);
   gpio.write(19, 1, function(err){
     if(err) throw err;
-    mqtt.publish('coffeefy/messages', 'On Knopf gedrueckt');
+    client.publish('coffeefy/messages', 'On Knopf gedrueckt');
     console.log('On Knopf gedrueckt');
   });
   setTimeout(500);
   gpio.write(19, 0, function(err){
     if(err) throw err;
-    mqtt.publish('coffeefy/messages', 'On Knopf losgelassen. Maschine laeuft');
+    client.publish('coffeefy/messages', 'On Knopf losgelassen. Maschine laeuft');
     console.log('On Knopf losgelassen. Maschine laeuft.');
   });
 }
@@ -81,12 +81,12 @@ function makeSmallCoffee(){
   gpio.setMode(MODE_BCM);
   gpio.setup(13, gpio.DIR_OUT);
   gpio.write(13, 1, function(err){
-    mqtt.publish('coffeefy/messages', '1Cup Button gedrueckt');
+    client.publish('coffeefy/messages', '1Cup Button gedrueckt');
     console.log('1Cup Button gedrueckt');
   });
   setTimeout(500);
   gpio.write(13, 0, function(err){
-    mqtt.publish('coffeefy/message', '1Cup Button losgelassen');
+    client.publish('coffeefy/message', '1Cup Button losgelassen');
     console.log('1Cup Button losgelassen');
   });
   wait(40);
@@ -99,12 +99,12 @@ function makeBigCoffee(){
   gpio.setMode(MODE_BCM);
   gpio.setup(26, gpio.DIR_OUT);
   gpio.write(26, 1, function(err){
-    mqtt.publish('coffeefy/messages', '2Cup Button gedrueckt');
+    client.publish('coffeefy/messages', '2Cup Button gedrueckt');
     console.log('2Cup Button gedrueckt');
   });
   setTimeout(500);
   gpio.write(26, 0, function(err){
-    mqtt.publish('coffeefy/messages', '2Cup Button losgelassen');
+    client.publish('coffeefy/messages', '2Cup Button losgelassen');
     console.log('2Cup Button losgelassen');
   });
   wait(40);
@@ -113,7 +113,7 @@ function makeBigCoffee(){
 }
 function wait(time){
   for(var i = time; i == 0; i--){
-    mqtt.publish('coffeefy/messages', 'waiting.. ' + i);
+    client.publish('coffeefy/messages', 'waiting.. ' + i);
     console.log('waiting.. ' + i);
     setTimeout(1000);
   }
