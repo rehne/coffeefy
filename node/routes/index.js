5a6ccfc90c174d91a8816288ea5c5034fa616b74
rehne <rene.honnen@smail.th-koeln.de>
2016-06-29 13:54:24 +0200

    .

--- node/routes/index.js
+++ node/routes/index.js
@@ -48,19 +48,11 @@ router.get('/python/makeBigCoffee', function(req, res, next){
 // NODE.JS
 // GET resource "/node/powerButton" aka run powerbutton test */
 router.get('/node/powerButton', function(req, res, next){
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   pressPowerButton();
-=======
-=======
->>>>>>> parent of 28023fd... node testing again
-=======
   gpio.setMode(MODE_BCM);
   gpio.setup(19, gpio.DIR_OUT);
->>>>>>> parent of ad78f1a... node testing again
+  gpio.setup(35, gpio.DIR_OUT);
   console.log('Test');
->>>>>>> parent of 28023fd... node testing again
   res.end();
 });
 // GET resource "/node/makeSmallCoffee" aka make small coffee
