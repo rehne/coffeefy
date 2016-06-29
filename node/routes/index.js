ad78f1aab558b394dae5206416c0377565d66b72
rehne <rene.honnen@smail.th-koeln.de>
2016-06-29 10:46:47 +0200

    node testing again

--- node/routes/index.js
+++ node/routes/index.js
@@ -48,8 +48,7 @@ router.get('/python/makeBigCoffee', function(req, res, next){
 // NODE.JS
 // GET resource "/node/powerButton" aka run powerbutton test */
 router.get('/node/powerButton', function(req, res, next){
-  gpio.setMode(MODE_BCM);
-  gpio.setup(19, gpio.DIR_OUT);
+  gpio.setup(35, gpio.DIR_OUT);
   console.log('Test');
   res.end();
 });
