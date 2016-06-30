e253187f079dd119e9b92a62a52b5ad51819cb61
kanonenfutter <van.kanonenfutter@web.de>
2016-06-30 21:11:32 +0200

    changed topic

--- python/mqttsubscriber.py
+++ python/mqttsubscriber.py
@@ -6,7 +6,8 @@ def on_connect(client, userdata, rc):
 	print("Connected with result code "+str(rc))
 # Subscribing in on_connect() means that if we lose the connection and
 # reconnect then subscriptions will be renewed.
-	client.subscribe("coffeefy/messages")
+	#client.subscribe("coffeefy/messages")
+	client.subscribe("coffeefy/sensors/ultrasonic")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
